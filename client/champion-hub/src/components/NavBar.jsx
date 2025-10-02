60faa0d527a5da61dc6e211c564bc1f9dc611ce5
abng01 <alicia.bang404@gmail.com>
2025-10-02 11:17:19 +1000

    added client folder

---
+++ client/champion-hub/src/components/NavBar.jsx
@@ -0,0 +1,101 @@
+import { AppBar, Toolbar, Box, IconButton, Button, TextField, Menu, MenuItem } from "@mui/material"
+import { NavLink } from "react-router-dom"
+import PersonIcon from "@mui/icons-material/Person"
+import logo from "../assets/League of Legends Icon.png"
+import { activeButton, buttonStyle } from "../themes/Theme"
+import * as React from 'react'
+import { useUser } from '../components/context/UserContext'
+
+export default function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const [randomNum, setRandomNum] = React.useState(1)
+  const { user, logout, isAuthenticated } = useUser()
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
+  const num = () => setRandomNum(Math.floor(Math.random() * 171 + 1))
+
+  return (
+    <AppBar position="fixed" sx={{ backgroundColor: "#1F2638", minHeight: "100px", justifyContent: "center"  }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between"}}>
+        {/* Left side */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <NavLink to="/dashboard">
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: "100px" }}
+            />
+          </NavLink>
+          <Button component={NavLink} to="/browse" style={({ isActive }) => (isActive ? activeButton : undefined)} sx={buttonStyle}>
+            Browse
+          </Button>
+          <Button component={NavLink} to={`/view/${randomNum}`} onClick={() => num()} style={({ isActive }) => (isActive ? activeButton : undefined)} sx={buttonStyle}>
+            Random
+          </Button>
+        </Box>
+
+        {/* Center (Searchbar) */}
+        <Box sx={{ flexGrow: 1, maxWidth: 500, mx: 2 }}>
+          <TextField
+            fullWidth
+            size="small"
+            placeholder="Search champions..."
+            sx={{
+              backgroundColor: "white",
+              borderRadius: 5,
+              "& fieldset": { border: "none" }
+            }}
+          />
+        </Box>
+
+        {/* Right side */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Button component={NavLink} to="/archive" style={({ isActive }) => (isActive ? activeButton : undefined)} sx={buttonStyle}>
+            Archive
+          </Button>
+
+          <IconButton onClick={handleClick}>
+            <PersonIcon sx={{ fontSize: 40, color: "#d0c178ff" }} />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            disableScrollLock
+          >
+            {!isAuthenticated ? (
+              [
+                <MenuItem onClick={handleClose} component={NavLink} to="/signup">Sign up</MenuItem>,
+                <MenuItem onClick={handleClose} component={NavLink} to="/login">Login</MenuItem>
+              ]
+            ) : (
+              [
+                <MenuItem onClick={handleClose} component={NavLink} to="/profile">My profile</MenuItem>,
+                <MenuItem onClick={() => {
+                  logout()
+                  handleClose()
+                  }}>Login</MenuItem>
+                ]
+            )}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+}
---
+++ client/champion-hub/src/pages/LaunchPage.jsx
@@ -0,0 +1,38 @@
+import lolText from "../assets/9eb028de391e65072d06e77f06d0955f66b9fa2c-736x316.png"
+import { Box, Button, Grid } from "@mui/material"
+import { Navigate, useNavigate } from "react-router-dom"
+import { buttonStyle } from "../themes/Theme"
+
+export default function Launchpage({onEnter}) {
+    const navigate = useNavigate()
+
+    const handleBrowse = () => {
+        onEnter()
+        navigate("/browse")
+    } 
+
+    const handleLogin = () => {
+        onEnter()
+        navigate("/login")
+    } 
+
+    return (
+        <>
+            <Box sx={{ display: "flex", gap: 6, alignItems: "center", minHeight: "100%", position: "fixed", left: "200px" }}>
+                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+                    <img src={lolText} alt="LoL Text" style={{ width: "500px" }}/>
+                    <h1 style={{ margin: "5px", fontSize: "62px", color: "#D8BF78" }}>Champions Hub</h1>
+                </Box>
+
+                <Box>
+                    <Box sx={{ display: "flex", gap: 4 }}>
+                        <Button onClick={handleBrowse} sx={{...buttonStyle, fontSize: "17px", py: 1.5, width: "300px", boxShadow: "0px 6px 14px #151412ff"}}>Browse Champions</Button>
+                        <Button onClick={handleLogin} sx={{...buttonStyle, fontSize: "17px", py: 1.5, width: "300px", boxShadow: "0px 6px 14px #151412ff"}}>Login</Button>
+                    </Box>
+                </Box>
+            </Box>
+        </>
+    )
+}
+
+// {{ backgroundColor: "#B38A43", px: 4, color: "#FFF2B4", textTransform: "capitalize", fontWeight: "bold", fontSize: "17px", py: 1.5, width: "300px", boxShadow: "0px 6px 14px #151412ff" }
