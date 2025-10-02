dc3723ce7c25b83d4b70a95713e36e64e8556b46
abng01 <alicia.bang404@gmail.com>
2025-10-02 10:56:38 +1000

    Start fresh

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    userController: require('./userController'),
-    championController: require('./championController'),
-    abilityController: require('./abilityController'),
-    listController: require('./listController'),
-    initialController: require('./initialController'),
-    authController: require('./authController'),
-    noteController: require('./noteController')
-}
